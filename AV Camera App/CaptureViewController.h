b1c4785bf45eac234c95de3282285800765bf63d
chrisechanique <chrisechanique@airbears2-10-142-6-212.airbears2.1918.berkeley.edu>
2014-02-21 16:40:15 -0800

    added the capture view controller

---
+++ AV Camera App/CaptureViewController.h
@@ -0,0 +1,13 @@
+//
+//  CaptureViewController.h
+//  OcularCellscope
+//
+//  Created by Chris Echanique on 2/21/14.
+//  Copyright (c) 2014 NAYA LOUMOU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaptureViewController : UIViewController
+
+@end
---
+++ AV Camera App/CaptureViewController.m
@@ -0,0 +1,75 @@
+//
+//  CaptureViewController.m
+//  OcularCellscope
+//
+//  Created by Chris Echanique on 2/21/14.
+//  Copyright (c) 2014 NAYA LOUMOU. All rights reserved.
+//
+
+#import "CaptureViewController.h"
+@import AVFoundation;
+
+@interface CaptureViewController ()
+@property (strong, nonatomic) AVCaptureSession *session;
+@property (strong, nonatomic) AVCaptureDevice *device;
+@property (strong, nonatomic) AVCaptureDeviceInput *deviceInput;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer *previewLayer;
+
+
+@end
+
+@implementation CaptureViewController
+
+@synthesize session, device, deviceInput, previewLayer;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self videoSetup];
+}
+
+-(void)videoSetup
+{
+    session = [[AVCaptureSession alloc] init];
+    [session setSessionPreset:AVCaptureSessionPresetPhoto];
+    
+    device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    
+    deviceInput = [AVCaptureDeviceInput deviceInputWithDevice:device error:Nil];
+    if ( [session canAddInput:deviceInput] )
+        [session addInput:deviceInput];
+    
+    previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:session];
+    [previewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    
+    
+    CALayer *rootLayer = [[self view] layer];
+    [rootLayer setMasksToBounds:YES];
+    [previewLayer setFrame:CGRectMake(-70, 0, rootLayer.bounds.size.height, rootLayer.bounds.size.height)];
+    [rootLayer insertSublayer:previewLayer atIndex:0];
+    
+    //previewLayer.affineTransform = CGAffineTransformInvert(CGAffineTransformMakeRotation(M_PI));
+    
+    
+    
+    [session startRunning];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
