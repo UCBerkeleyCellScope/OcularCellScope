b992619cf1e001b50da534e1b1358719795e1055
pjloury <pjloury@gmail.com>
2014-05-03 20:14:13 -0700

    UIColor+Custom

---
+++ AV Camera App/UIColor+Custom.h
@@ -0,0 +1,17 @@
+//
+//  UIColor+Custom.h
+//  OcularCellscope
+//
+//  Created by PJ Loury on 4/28/14.
+//  Copyright (c) 2014 Ocular CellScope. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIColor (Custom)
++ (UIColor *)colorWithR:(CGFloat)red G:(CGFloat)green B:(CGFloat)blue A:(CGFloat)alpha;
++ (UIColor *)lightGreenColor;
++ (UIColor *)mediumGreenColor;
++ (UIColor *)darkGreenColor;
+@end
+
