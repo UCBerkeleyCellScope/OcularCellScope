0d04b34b64b3f368c6bbac40a331e83e867a94f3
pjloury <pjloury@gmail.com>
2014-06-03 16:43:08 -0700

    Commit Settings

--- Constants.h
+++ Constants.h
@@ -17,7 +17,24 @@
 //static NSString * const BaseURLString = @"http://ec2-54-186-247-188.us-west-2.compute.amazonaws.com/";
 //This needs to be changed
 
+#define ACCESS_KEY_ID          @"AKIAIZJCDD43UTMHGLXQ"
+#define SECRET_KEY             @"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"
+
+// Constants for the Bucket and Object name.
+#define PICTURE_BUCKET         @"picture-bucket"
+#define PICTURE_NAME           @"NameOfThePicture"
+
+
+#define CREDENTIALS_ERROR_TITLE    @"Missing Credentials"
+#define CREDENTIALS_ERROR_MESSAGE  @"AWS Credentials not configured correctly.  Please review the README file."
+
+
 @interface Constants : NSObject
+
+/**
+ * Utility method to create a bucket name using the Access Key Id.  This will help ensure uniqueness.
+ */
++(NSString *)pictureBucket;
 
 
 @end
